fb701efa581f21fe31837daf359463177c84d455
169mias <169z87mn@gmail.com>
2018-11-23 23:38:23 +0900

    remove drawer

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -1,35 +1,8 @@
 <template>
   <header>
-    <v-navigation-drawer
-      class="lighten-3"
-      dark
-      temporary
-      absolute
-      v-model="navBar"
-    >
-      <v-list>
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-toolbar dark>
-      <v-toolbar-side-icon @click.stop="navBar = !navBar"></v-toolbar-side-icon>
       <v-toolbar-title class="white--text">169z87mn.xyz</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
     </v-toolbar>
   </header>
 </template>
@@ -39,12 +12,7 @@ export default {
   name: 'NaviBar',
   data () {
     return {
-      navBar: null,
-      items: [
-        { title: 'Dashboard', icon: 'dashboard' },
-        { title: 'Account', icon: 'account_box' },
-        { title: 'Admin', icon: 'gavel' }
-      ]
+      navBar: null
     }
   }
 }
